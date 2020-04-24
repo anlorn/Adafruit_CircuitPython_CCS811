22c2995e9b1c93264b3da460ac5d9b49a4e99014
Anton Tinyakov <anlorn@anlorn.com>
2020-04-23 23:11:52 -0400

    Test Basline usage

--- examples/ccs811_simpletest.py
+++ examples/ccs811_simpletest.py
@@ -6,6 +6,12 @@ import adafruit_ccs811
 i2c = busio.I2C(board.SCL, board.SDA)
 ccs811 = adafruit_ccs811.CCS811(i2c)
 
+try:
+    f = open("baseline", "r")
+    current_baseline = int(f.read())
+except:
+    current_baseline = 99999999
+
 # Wait for the sensor to be ready
 while not ccs811.data_ready:
     pass
@@ -13,3 +19,11 @@ while not ccs811.data_ready:
 while True:
     print("CO2: {} PPM, TVOC: {} PPB".format(ccs811.eco2, ccs811.tvoc))
     time.sleep(0.5)
+    new_baseline = ccs811.baseline
+    if new_baseline < current_baseline:
+        f = open("baseline", "w")
+        f.write(str(new_baseline))
+        f.close()
+        current_baseline = new_baseline
+        print("write")
+    print(current_baseline)
